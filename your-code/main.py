57bfaa155838793c8df94a2724c9020340742873
Cristian Baquero <cristiandbaqueros@gmail.com>
2021-03-17 12:05:12 +0100

    Solution Lab Numpy by Cristian Baquero

--- your-code/main.py
+++ your-code/main.py
@@ -1,69 +1,80 @@
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-
+print(np.__version__)
+print(np.show_config())
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a = np.random.rand(2,3,5)
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.ones((5,2,3))
 
 #6. Print b.
 
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
-
+if (a.shape == b.shape):
+    print('a and b have the same size')
+else:
+    print('a and b does not have the same size')
 
 #8. Are you able to add a and b? Why or why not?
 
+#It is not possible to operate between different-size arrays.
 
+#9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to variable "c".
 
-#9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
+c = np.transpose(b, (1, 2, 0))
 
+#10. Try to add a and c. Now it should work. Assign the sum to variable "d". But why does it work now?
 
+d = a + c
 
-#10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
-
+#Any arithmetic operations between equal-size arrays applies the operation elementwise.
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
+print(a)
 
+print(d)
 
+#The difference between these two arrays is due to the sum of c to a by adding 1 to each element of a. Appart from that, d and a share the same dimentions
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e = a * c
 
 #13. Does e equal to a? Why or why not?
 
-
+#e is equal to a because c is an array with all values equaling 1, furthermore when a is multiply to c, the result (e) has the same values than a
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_max = d.max()
 
+d_min = d.min()
+
+d_mean = d.mean()
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
-
+f = np.empty([2, 3, 5])
 
 """
 #16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
@@ -74,7 +85,6 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
-
 
 
 
