34b76e56b981505719f63c8d537b2abe43108015
acamposla <alejandro.campos@imprexeurope.com>
2021-03-17 21:16:12 +0100

    numpy

--- your-code/main.py
+++ your-code/main.py
@@ -1,79 +1,173 @@
 #1. Import the NUMPY package under the name np.
-
+import numpy as np 
+import random
 
 
 #2. Print the NUMPY version and the configuration.
 
+print(np.__version__)
 
 
-#3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
-# Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
+
+#3. Generate a 2x3x5 3-dimensional array with random values. 
+#Assign the array to variable "a"
+# Challenge: there are at least three easy ways that use numpy 
+#to generate random arrays. 
+#How many ways can you find?
+
+a = np.random.randint(low=50, high=60, size=(2, 3, 5))
+
+print(a)
+
+a = np.random.standard_normal((2, 3, 5))
+print(a)
+
+a = np.random.random((2, 3, 5))
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+print(a)
 
 
 
 #4. Print a.
 
+print(a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
 
+b = np.ones((5, 2, 3))
+
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+
+
 
 #6. Print b.
 
 
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
+print(b.size != a.size)
 
 
 
 #8. Are you able to add a and b? Why or why not?
 
+#add_ = np.add(a, b)
+#operands could not be broadcast together with shapes
+#nope, cause they have different shapes. 
 
+#9. Transpose b so that 
+#it has the same structure of a (i.e. become a 2x3x5 array).
+# Assign the transposed array to varialbe "c".
 
-#9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+c = b.reshape(2,3,5)
+print(c)
 
+#10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". 
+#But why does it work now?
 
+d = a + c
+print(d)
+#cause they are same size
 
-#10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
+#11. Print a and d. Notice the difference and 
+#relation of the two array in terms of the values? Explain.
 
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+print (a)
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+print(d)
 
-
-#11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
 
 
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e = a *c
 
 #13. Does e equal to a? Why or why not?
 
+print(e.size != a.size)
 
 
 
-#14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
+#14. Identify the max, min, and mean values in d. 
+#Assign those values to variables "d_max", "d_min", and "d_mean"
+
+
+d_max = d.max()
+
+d_min = d.min()
+
+d_mean = d.mean()
+
+#15. Now we want to label the values in d. 
+# First create an empty array "f" 
+# with the same shape (i.e. 2x3x5) 
+# as d using `np.empty`.
+
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+
+f = np.empty((2, 3, 5), dtype =int)
+print(f)
 
 
 
-
-#15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
-
+#label de values?????
 
 
 """
-#16. Populate the values in f. For each value in d, if it's larger than d_min but smaller than d_mean, assign 25 to the corresponding value in f.
-If a value in d is larger than d_mean but smaller than d_max, assign 75 to the corresponding value in f.
+#16. Populate the values in f. For each value in d, if it's 
+larger than d_min but smaller than d_mean, assign 25 to the corresponding 
+value in f.
+If a value in d is larger than d_mean but smaller than d_max,
+ assign 75 to the corresponding value in f.
 If a value equals to d_mean, assign 50 to the corresponding value in f.
 Assign 0 to the corresponding value(s) in f for d_min in d.
 Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
+
+_1_ = (d > d_min) & (d < d_mean)
+f[_1_] = 25
+
+_2_ = (d > d_mean) & (d < d_max)
+
+f[_2_] = 75
+
+_3_ = (d == d_mean)
+f[_3_] = 50
+
+_4_ = (d == d_min)
+f[_4_] = 0
+
+_5_ = (d == d_max)
+f[_5_] = 100
 
 
 
